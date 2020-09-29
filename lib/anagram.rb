cb0b039f50b13aef70cfb557b7607a25a3ffd96a
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-29 20:51:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  
+  attr_accessor :anagrams
+  
+  def initialize(word)
+    @anagrams = word.split("").sort.join
+  end
+  
+  def match(array)
+    array.each{|word| word.split("").sort.join}
+    if == @agangrams}
+    if true 
+  end
+  
+end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlist google inlets banana))
